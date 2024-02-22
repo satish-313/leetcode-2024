2ba0e63feadec55fc1965a6a024d6132e465a44b
satish-313 <pradhansatish53@gmail.com>
2024-02-22 22:51:58 +0530

    day 22

---
+++ feb/find-the-town-judge.go
@@ -0,0 +1,26 @@
+package feb
+
+import (
+	"fmt"
+)
+
+func findJudge(n int, trust [][]int) int {
+	l, r := make([]int, n+1), make([]int, n+1)
+
+	for _, t := range trust {
+		l[t[0]]++
+		r[t[1]]++
+	}
+	for i := 1; i <= n; i++ {
+		if l[i] == 0 && r[i] == n-1 {
+			return i
+		}
+	}
+	return -1
+}
+
+func Test22() {
+	n := 4
+	trust := [][]int{{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}}
+	fmt.Println(findJudge(n, trust))
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"leet2024/feb"
+)
+
+func main() {
+	fmt.Println("leet code 2024")
+	feb.Test22()
+}
